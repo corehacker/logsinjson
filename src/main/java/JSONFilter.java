d75bd46928c2eab56e1472802b7589d824548b94
Sandeep Prakash <123sandy@gmail.com>
2019-03-17 23:01:55 -0700

    Some update.

--- src/main/java/JSONFilter.java
+++ src/main/java/JSONFilter.java
@@ -1,13 +1,37 @@
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intellij.execution.filters.Filter;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
 
 public class JSONFilter implements Filter {
 
     @Nullable
     @Override
     public Result applyFilter(String line, int entireLength) {
+
+        String selectedText = line;
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String r;
+
+        try {
+            Object json = mapper.readValue(line, Object.class);
+            r = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        } catch (JsonProcessingException e1) {
+            r = selectedText;
+        } catch (IOException e1) {
+            r = selectedText;
+        }
+
+        System.out.println(r);
+
+        Result result = new Result();
+
         return null;
     }
 }
